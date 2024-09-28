2a4eaa0b679690d836fafa81496d995445d6899e
filipe-fff <pintovicentinifilipe@gmail.com>
2024-09-27 22:58:07 -0300

    componente de informações de item criado

--- projeto-reactive-forms/src/app/components/components.module.ts
+++ projeto-reactive-forms/src/app/components/components.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { UsersListComponent } from './users-list/users-list.component';
+import { GeneralInformationsComponent } from './general-informations/general-informations.component';
+import { UserInfosItemComponent } from './user-infos-item/user-infos-item.component';
 
 
 
 @NgModule({
   declarations: [
     UsersListComponent,
+    GeneralInformationsComponent,
+    UserInfosItemComponent,
   ],
   imports: [
     CommonModule,
@@ -15,6 +19,7 @@ import { UsersListComponent } from './users-list/users-list.component';
   ],
   exports: [
     UsersListComponent,
+    GeneralInformationsComponent,
   ]
 })
 export class ComponentsModule { }
---
+++ projeto-reactive-forms/src/app/components/user-infos-item/user-infos-item.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-infos-item',
+  templateUrl: './user-infos-item.component.html',
+  styleUrl: './user-infos-item.component.scss'
+})
+export class UserInfosItemComponent {
+  @Input({ required: true }) title!: string;
+  @Input({ required: true }) description!: string;
+}
