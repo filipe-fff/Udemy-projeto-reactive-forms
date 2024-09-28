023e64904cd40016024cbeedc1c946c0049e3d2b
filipe-fff <pintovicentinifilipe@gmail.com>
2024-09-27 21:51:16 -0300

    container feito

--- projeto-reactive-forms/src/app/app.module.ts
+++ projeto-reactive-forms/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AngularMaterialModule } from './angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AngularMaterialModule,
     HttpClientModule,
   ],
   providers: [
