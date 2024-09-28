a049688d1b487fc8e338cd270e609d4ac8538a14
filipe-fff <pintovicentinifilipe@gmail.com>
2024-09-27 21:37:15 -0300

    modulo do angular material e tailwindcss criado

---
+++ projeto-reactive-forms/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    './src/**/*.{html,ts}',  // Inclui os arquivos de template e componentes Angular
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
