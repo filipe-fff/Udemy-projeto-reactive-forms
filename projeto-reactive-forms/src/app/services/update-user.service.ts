3ff58c9235a1acfa9dcac641aaf7173e264af855
filipe-fff <pintovicentinifilipe@gmail.com>
2024-10-15 23:43:18 -0300

    criação do service de novo usuário

---
+++ projeto-reactive-forms/src/app/services/update-user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { map, Observable } from "rxjs";
+import { IUser } from "../interfaces/users/user.interface";
+
+@Injectable({
+    providedIn: "root",
+})
+export class UpdateUserService {
+
+    getUpdateUser(newUser: IUser): Observable<IUser> {
+        return new Observable<{ status: number, body: IUser }>((observable) => {
+            observable.next({ status: 200, body: structuredClone(newUser)});
+            observable.complete();
+        })
+        .pipe(map((updateUserResponse: { status: number, body: IUser }) => updateUserResponse.body));
+    }
+}
